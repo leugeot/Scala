283ba66265d2a49b38a454fd575107dd022dfda6
Pebin <petrbarton2@gmail.com>
2015-04-25 17:27:45 +0200

    Update main activity to ActionBarActivity, Refactor to fragments

---
+++ app/src/main/java/cz/kinoscala/scala/fragment/UpcomingMoviesFragment.java
@@ -0,0 +1,128 @@
+package cz.kinoscala.scala.fragment;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+import cz.kinoscala.scala.JSONLoader;
+import cz.kinoscala.scala.Movie;
+import cz.kinoscala.scala.MovieAdapter;
+import cz.kinoscala.scala.MovieParser;
+import cz.kinoscala.scala.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link UpcomingMoviesFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link UpcomingMoviesFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class UpcomingMoviesFragment extends Fragment {
+    private OnFragmentInteractionListener mListener;
+
+    ListView movieListView;
+    ProgressDialog progressDialog;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_upcoming_movies, container, false);
+        movieListView = (ListView) v.findViewById(R.id.movie_list);
+
+        return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        new MoviesDownloader().execute();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+    private class MoviesDownloader extends AsyncTask<Void, Void, List<Movie>> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setMessage("Downloading movies");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected List<Movie> doInBackground(Void... params) {
+            JSONLoader jsonLoader = new JSONLoader();
+
+            JSONObject jsonObject = jsonLoader.getJsonFromUrl("http://www.kinoscala.cz/1.0/export/showtimes");
+            return MovieParser.parse(jsonObject);
+        }
+
+        @Override
+        protected void onPostExecute(List<Movie> movies) {
+            super.onPostExecute(movies);
+            if (progressDialog.isShowing()) {
+                progressDialog.dismiss();
+            }
+
+            updateMovieListView(movies);
+        }
+    }
+
+    private void updateMovieListView(List<Movie> movies){
+        if (movies != null) {
+            ListAdapter adapter = new MovieAdapter(getActivity(), 0, movies);
+            movieListView.setAdapter(adapter);
+        }
+    }
+}
